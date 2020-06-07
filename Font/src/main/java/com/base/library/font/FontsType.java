910a5c456012ddf0e1ab40b164e7a7b91fc66e88
reber <reber2c@163.com>
2020-06-07 11:03:22 +0800

    create font library

---
+++ Font/src/main/java/com/base/library/font/FontsType.java
@@ -0,0 +1,26 @@
+package com.base.library.font;
+
+import android.content.Context;
+
+import androidx.annotation.IntDef;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+/**
+ * @author reber
+ * @see FontsUtil#getTypeFace(Context, int)
+ */
+@IntDef({
+        FontsType.ROBOTO_REGULAR,
+        FontsType.ROBOTO_ITALIC,
+        FontsType.ROBOTO_BOLD
+})
+//Only judge in parameters, it will be discarded after compilation
+@Retention(RetentionPolicy.SOURCE)
+public @interface FontsType {
+
+    int ROBOTO_REGULAR = 0;
+    int ROBOTO_ITALIC = 1;
+    int ROBOTO_BOLD = 2;
+}
